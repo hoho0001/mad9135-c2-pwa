34bc0ba41cc070fc1fab6fff7863f38ccfa984a7
HoHoangLien <hoho0001@algonquinlive.com>
2019-11-19 13:22:40 -0500

    Update files for Audits

--- sw.js
+++ sw.js
@@ -15,7 +15,16 @@ workbox.precaching.precacheAndRoute([
     'js/main.js',
     'js/StarWarsApiService.js',
     'css/style.css',
-    'image/offline.jpg'
+    'images/offline.jpg',
+    'images/icons/icon-72x72.png',
+    "images/icons/icon-128x128.png",
+    "images/icons/icon-144x144.png",
+    "images/icons/icon-152x152.png",
+    "images/icons/icon-192x192.png",
+    "images/icons/icon-384x384.png",
+    "images/icons/icon-512x512.png",
+    "images/icons/icon-96x96.png",
+
 ],
 {
   directoryIndex: null,
